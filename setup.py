d6d280a604625e15a16f3b306829d4726604f7c5
PaulChanHK <19663576+PaulChanHK@users.noreply.github.com>
2024-02-08 18:15:23 +0800

    Create publish workflow
    Signed-off-by: PaulChanHK <19663576+PaulChanHK@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, Extension
 
 setup(
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     ext_modules=[
         Extension(
             'fastlz',
